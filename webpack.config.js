0ff6d8a60e0e6bcc85141bfdf57357dfb643493c
soonoo <qpseh2m7@gmail.com>
2017-12-15 19:11:45 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
